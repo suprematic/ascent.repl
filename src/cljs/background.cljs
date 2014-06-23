982eb436e39f622e6d81c250d9992ebd89ad34bb
Alexey Aristov <aav@acm.org>
2014-06-23 18:36:24 +0200

    work in progress

--- src/cljs/background.cljs
+++ src/cljs/background.cljs
@@ -1,14 +1,14 @@
 (ns background
 	(:require
 		[cljs.core.async :as async]
+  	[clojure.walk :as walk]
 		[khroma.log :as log]
+		[khroma.runtime :as runtime]
+
 		[goog.string :as gstring]
 		[clojure.string :as s])
 	(:require-macros
 		[cljs.core.async.macros :refer [go go-loop]]))
-
-(defn format [fmt & args]
-  (apply gstring/format fmt args))
 
 (def debug
 	(atom true))
@@ -19,122 +19,148 @@
 (def connections
 	(atom {}))
 
-(defn- put-in-map [map-atom string-key value]
-	(swap! map-atom assoc (keyword string-key) value))
-
-(defn- remove-from-map [map-atom string-key]
-	(swap! map-atom dissoc (keyword string-key)))
-
-(defn- get-from-map [map-atom string-key]
-	((keyword string-key) @map-atom))
-
-(defn set-port [destination tab-id port]
-	{:pre [(string? tab-id), (string? destination)]}
-	(if-not ((keyword destination) connections)
-		(put-in-map connections tab-id (assoc port (keyword tab-id) tab-id))))
-
-(defn set-tab-info [tab-id info]
-	{:pre [(string? tab-id)]}
-	(put-in-map tab-infos tab-id info))
-
-(defn get-tab-info [tab-id]
-	{:pre [(string? tab-id)]}
-	(get-from-map tab-infos tab-id))
-
-(defn remove-tab-info [tab-id]
-	{:pre [(string? tab-id)]}
-	(remove-from-map tab-infos tab-id))
+(defn set-port! [destination tab-id port]
+	(let [path [(str destination) (str tab-id)]]
+  	(swap! connections
+    	#(assoc-in % path port))))
 
 (defn get-port [destination tab-id]
-	{:pre [(string? tab-id), (string? destination)]}
-	(if-let [for-destination (get-from-map connections destination)]
-		(get-from-map for-destination tab-id)
-		nil))
+ 	(get-in @connections [(str destination) (str tab-id)]))
 
-(defn remove-port [destination tab-id]
-	{:pre [(string? tab-id), (string? destination)]}
-	(put-in-map connections destination (dissoc (get-from-map destination connections) tab-id)))
-
-(defn set-local-port [destination handler]
-	{:pre [(string? destination)]}
-	(set-port "background" destination {:post-message handler :tab-id "*"}))
+(defn remove-port! [destination tab-id]
+  (swap! connections
+    #(update-in % [(str destination)] dissoc (str tab-id))))
+  
+(defn set-local-port! [destination handler]
+	(set-port! "background" destination handler))
 
 (defn get-local-port [destination]
-	{:pre [(string? destination)]}
-	(get-port "background", destination))
+	(get-port "background" destination))
 
-(defn connect [connection destination tab-id]
-	(set-port destination tab-id connection)
-	(let [local-on-message (fn [message sender send-response]
-		(if-let [port (fn []
-			(if (= "background" (:destination message))
-				(get-local-port (:type message))
-				(get-port (:destination message) tab-id)))]
-			(.postMessage port (assoc message :source destination :source-tab-id tab-id))
-			(if (@debug) (if ("log" not= (:type message)) (log/debug (format "message %s:%s -> /dev/null" destination tab-id message)))))) on-disconnect (fn [connection]
-		(do
-			(if (@debug) (log/debug (format "desconnecting %s:%s" destination tab-id)))
-			(.removeListener js/connection.onMessage local-on-message)
-			(remove-port destination tab-id)))]
-		(.addListener js/connection.onMessage local-on-message)
-		(.addListener js/connection.onDisconnect on-disconnect)))
+(defn set-tab-info! [tab-id info]
+  (swap! tab-infos assoc (str tab-id) info))
+
+(defn get-tab-info [tab-id]
+  (@tab-infos (str tab-id)))
+
+(defn remove-tab-info! [tab-id]
+  (swap! tab-infos dissoc (str tab-id)))
+
+(defn connect [port source tabId]
+	(set-port! source tabId 
+    (fn [message] 
+      (log/debug "sending message to %s:%s" source tabId message)
+      (async/put! port message)))
+ 
+ 	(go-loop []
+  	(if-let [message (<! port)]
+     	(let [message (walk/keywordize-keys message) {:keys [type destination]} message background? (= destination "background")]
+        (log/debug "received message: " message " background: " background?)
+    		(if-let [port-fn (if background?
+	                     	   (get-local-port type)
+	                       	 (get-port destination tabId))]
+          (do       
+	        	(when (and @debug (not= type "log"))
+	          	(log/debug "message %s:%s -> %s:%s" source tabId destination (if-not background? tabId "*")) message)
+	        
+		      	(port-fn
+		        	(assoc message :source source :sourceTabId tabId)))
+          
+          (log/debug "message %s:%s -> /dev/null" source tabId))
+      	(recur))
+      
+     	(remove-port! source tabId))))
 
 
-(defn boot []
-	(log/debug "Just for boot checking..."))
+; move to khroma
+(defn tab-updated-events []
+  (let [ch (async/chan)]
+    (.addListener js/chrome.tabs.onUpdated
+      (fn [id info tab]
+        (async/put! ch (walk/keywordize-keys (js->clj {:tabId id :changeInfo info :tab tab}))))) ch))
+
 
 (defn init []
-	(boot)
+	(log/debug "Just for boot checking...")
 
-	(.addListener js/chrome.runtime.onConnect
-		(fn [connection]
-			(let [parts (s/split (:name connection) ":")]
-				(if (= 2 (count parts))
-					(do
-						(if (@debug) (log/debug (format "incoming connection from %s" (:name connection))))
-						(connect connection (get parts 0) (get parts 1))
-						(if-let [tab-info (get-tab-info (get parts 1))]
-							(.postMessage connection {:type "tab-info" :info tab-info})))))))
+ 	(let [ch (runtime/connections)]
+  	(go-loop []
+    	(when-let [connection (<! ch)]
+       	(let [connection-name (runtime/port-name connection) parts (s/split connection-name ":")]
+					(when (= 2 (count parts))
+       			(let [destination (first parts) tabId (second parts)]
+							(when @debug
+	        			(log/debug (str "incoming connection from " connection-name)))
+	      
+							(connect connection destination tabId)
+	      
+							(when-let [tab-info (get-tab-info tabId)]
+								(async/put! connection {:type "tab-info" :info tab-info})))))
 
-	(set-local-port "log"
+        (recur))))
+
+	(set-local-port! "log"
 		(fn [message]
-			(if (@debug) (log/debug "*** " (:text message)))))
+			(if @debug (log/debug "*** " (:text message)))))
 
-	(set-local-port "inject-agent"
-		(fn [message]
-			(if (@debug) (log/debug "agent injection requested: " (:text message)))
-			(.executeScript js/chrome.tabs (:tab-id message) {:file "js/injected.js"}
+	(set-local-port! "inject-agent"
+		(fn [{:keys [tabId]} message]
+			(if @debug 
+     		(log/debug "agent injection requested for: " tabId))
+   
+			(.executeScript js/chrome.tabs tabId (clj->js {:file "js/injected.js"})
 				(fn [result]
-					(if (@debug) (log/debug (format "connecting to tab:%s" (:tab-id message))))
-					(connect (.connect js/chrome.tabs (:tab-id message) {:name (str "background:" (:tab-id message))}) "tab" (str (:tab-id message)))))))
+					(when @debug 
+       			(log/debug "connecting to tab:%s" tabId))
+     
+     			(let [port (runtime/channel-from-port 
+                     	  (.connect js/chrome.tabs tabId  (clj->js {:name (str "background:" tabId)})))]
+          
+          	(connect port "tab" tabId))))))
 
-	(set-local-port "tab-info"
-		(fn [message]
-			(if (@debug) (log/debug "background page received tab-info: " (:text message)))
-			(if (= "tab" (:source message))
-				(if-let [tab-info (get-tab-info (:source-tab-id message))]
-					(if-let [port (get-port "repl" (:source-tab-id message))]
-						(let [out {:type "tab-info" :info (assoc tab-info :agent-info (:agent-info message))}]
-							(if (@debug)
-								(do
-									(log/debug "sending tab-info to repl:" (:source-tab-id message) out)
-									(log/debug "port:" port)))
-							(.postMessage port out))
-						(log/warn (format "cannot find port repl:%s" (:source-tab-id message))))
-					(log/warn "no tab-info found for tab-info message: " (:text message))))))
+	(set-local-port! "tab-info"
+		(fn [{:keys [agentInfo source sourceTabId] :as message}]
+			(when @debug 
+     		(log/debug "background page received tab-info: " message))
+   
+			(if (= "tab" source)
+				(if-let [tab-info (get-tab-info sourceTabId)]
+					(if-let [port-fn (get-port "repl" sourceTabId)]
+						(let [out {:type "tab-info" :info (assoc tab-info :agentInfo agentInfo)}]
+   						(when @debug
+								(log/debug "sending tab-info to repl:" sourceTabId out))
+							(port-fn out))
+						(log/warn "cannot find port repl:%s" sourceTabId))
+					(log/warn "no tab-info found for tab-info message: " message)))))
 
-	(.addListener js/chrome.tabs.onUpdated
-		(fn [tab-id change-info tab]
-			(if (= "complete" (:status change-info))
-				(set-tab-info (str tab-id) {:agent-info nil :url (:url tab)})
-				(when (get-port "repl" (str tab-id))
-					(if (@debug) (log/debug (format "to repl:%s" (str tab-id) (get-tab-info (str tab-id)))))
-					(.postMessage (get-port "repl" (str tab-id)) {:type "tab-info" :info (get-tab-info (str tab-id))})))))
+ 
+	(let [ch (tab-updated-events)]
+		(go-loop []
+	  	(when-let [{:keys [tabId changeInfo tab]} (<! ch)]
+				(when (= "complete" (:status changeInfo))
+					(let [tab-info (get-tab-info tabId)]
+	  				(log/info "tab load complete" tabId)
+						(set-tab-info! tabId {:agentInfo nil :url (:url tab)})
 
+						(when-let  [port-fn (get-port "repl" tabId)]
+							(when @debug
+	      				(log/debug  "to repl:%s" tabId tab-info))
+	    
+							(port-fn {:type "tab-info" :info tab-info}))))
+	      
+	    	(recur))))
+ 
 	(.addListener js/chrome.tabs.onRemoved
 		(fn [tab-id remove-info]
-			(remove-tab-info (str tab-id))))
+			(remove-tab-info! tab-id)))
 
 	(.addListener js/chrome.tabs.onReplaced
 		(fn [added-tab-id removed-tab-id]
-			(remove-tab-info (str added-tab-id)))))
+			(remove-tab-info! added-tab-id))))
+
+
+
+
+
+
+
