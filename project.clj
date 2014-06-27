3ca4f9860146772de8beb82427e9db67522c7ca7
Ilya Shinkarenko <shinych@gmail.com>
2014-06-27 14:18:45 +0200

    fix mvn project group and name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cdtrepl.extension "0.1.0-SNAPSHOT"
+(defproject ascent/repl "0.1.0-SNAPSHOT"
   :description    "ClojureScript REPL for Chrome DevTools"
   :license {
       :name "Eclipse Public License"
